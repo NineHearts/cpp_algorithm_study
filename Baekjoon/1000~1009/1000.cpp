14b963cfbac77d96f1cc580d2abbea344b817695
NineHearts <46129253+NineHearts@users.noreply.github.com>
2023-08-30 15:24:40 +0900

    Solving

---
+++ Baekjoon/1000~1009/1000.cpp
@@ -0,0 +1,16 @@
+//백준 1000번 문제 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+#include<iostream>
+
+int main()
+{
+    using std::cout;
+    using std::cin;
+    
+    int a,b;
+
+    cin >> a >> b;   
+    cout << a+b;
+    
+    return 0;
+}
---
+++ Baekjoon/1000~1009/1001.cpp
@@ -0,0 +1,17 @@
+//백준 1000번 문제 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+
+#include<iostream>
+
+int main()
+{
+    using std::cout;
+    using std::cin;
+    
+    int a, b, result;
+
+    cin >> a>>b;   
+    result = a-b;
+    cout << result;
+    
+    return 0;
+}
