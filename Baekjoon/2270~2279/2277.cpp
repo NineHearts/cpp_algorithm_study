c021f378750b0d75a1393fe8286013c0428debc5
NineHearts <46129253+NineHearts@users.noreply.github.com>
2023-09-02 13:11:46 +0900

    Update 2277.cpp

--- Baekjoon/2270~2279/2277.cpp
+++ Baekjoon/2270~2279/2277.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
 #include <string>
 
-class Locker
+class Lock
 {
     private:
         short dial_n;
         short range_m;
-        short * number;
+        short *number;
         short number_length;
-        int center;
+        short *forward, *reverse;
     public:
-    
-        void init_locker(std::string n, std::string num);
-        void show_locker();
-        void get_center();
-        ~Locker();
+        void init_lock(std::string n, std::string num);
+        void show_lock();
+        void solve();
+        ~Lock();
 };
 
-void Locker::init_locker(std::string n, std::string num)
+void Lock::init_lock(std::string n, std::string num)
 {
 
     dial_n = atoi(n.substr(0, n.find(' ')).c_str());
     range_m = atoi(n.substr(n.find(' ') + 1, n.length()).c_str());
     number = new short[num.length()]();
     number_length = num.length();
+    forward = new short[num.length() - 1]();
+    reverse = new short[num.length() - 1]();
 
     for (int i = 0; i < num.length(); i++)
     {
@@ -31,11 +32,6 @@ void Locker::init_locker(std::string n, std::string num)
         std::cout << number[i] << std::endl;
     }
 
-    show_locker();
-}
-
-void Locker::show_locker()
-{
     using std::cout;
 
     cout << "count of dials : " << dial_n << std::endl;
@@ -43,18 +39,28 @@ void Locker::show_locker()
     cout << "setup number : ";
     for (int i = 0; i < number_length; i++)
         cout << number[i];
-    
 }
 
-void Locker::get_center()
+void Lock::show_lock()
 {
     for (int i = 0; i < number_length; i++)
+        std::cout << number[i];
+}
+
+void Lock::solve()
+{
+    int i;
+    int temp = 0;
+    for (i = 0; i < number_length - 1; i++)
     {
-        
+        if (temp == 0)
+            temp = number[i+1];
+        else
+            forward[i] = (temp < number[i+1]) ? (number[i+1] - temp) : (number[i+1] - temp) + 10 
     }
 }
 
-Locker::~Locker()
+Lock::~Lock()
 {
     delete[] number;
 }
@@ -62,18 +68,18 @@ Locker::~Locker()
 
 int main()
 {
-    using std::cout;
     using std::cin;
+    using std::string;
 
-    std::string n;
-    std::string m;
+    string n;
+    string m;
 
-    Locker lock;
+    Lock lock;
 
     getline(cin, n);
     getline(cin, m);
 
-    lock.init_locker(n, m);
+    lock.init_lock(n, m);
 
     return 0;
 }
